544b839121afe0952286a1f7a9367a58256f62cb
Nikolay Vergeles <Vergeles4Nikolay@gmail.com>
2023-03-31 23:38:18 +0300

    add cool

--- test/Main.java
+++ test/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String start = "Hello world";
         System.out.println(start);
+        System.out.println("cool");
 
     }
 }
